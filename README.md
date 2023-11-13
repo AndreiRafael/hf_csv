dce283f47582e2265f1bcec2eab8dfb27963832b
Andrei Rafael Brongel <abrongel@gmail.com>
2023-11-13 15:40:10 -0300

    split into .c file & removed custom allocators

--- README.md
+++ README.md
@@ -1,11 +1,8 @@
 # HF CSV
-HF CSV is a simple single-header csv utility written in C. It allows you to create, read, edit, and save csv files. Not supposed to be anything serious, more of a fun project.
-
-# Build
-Built using CMake, tested with MingW. There's a simple test program at main.c, where the library itself is included and its implementation defined.
+HF CSV is a simple csv utility written in C. It allows you to create, read, edit, and save csv files. Not supposed to be anything serious, more of a fun project.
 
 # Usage
-To use the library, include hf_csv.h. In one of your source files, HF_CSV_IMPLEMENTATION should be defined before including, so that the implementation is then defined there, like you'd do with a stb header.
+To use the library, include hf_csv.h and compile hf_csv.c with your other source files or link it as a library.
 
 # TODO:
 - Complete Usage section of readme
--- src/main.c
+++ src/main.c
@@ -3,130 +3,11 @@
 #include <string.h>
 #include <assert.h>
 
-#define HF_CSV_IMPLEMENTATION
 #include "hf_csv.h"
-
-#define USE_TEST_ALLOCATOR
-//#define TEST_ALLOCATOR_VERBOSE
-
-#ifdef USE_TEST_ALLOCATOR
-
-#ifdef TEST_ALLOCATOR_VERBOSE
-#define TEST_ALLOCATOR_RUN(code) code
-#else
-#define TEST_ALLOCATOR_RUN(code)
-#endif
-
-typedef struct Allocation_s {
-    void* adress;
-    size_t size;
-} Allocation;
-
-#define ALLOCATOR_MAX_ALLOCS 2048
-
-typedef struct Allocator_s {
-    Allocation* allocations;
-    size_t allocation_count;
-    size_t full_size;
-} Allocator;
-
-static void* allocator_malloc(void* allocator, size_t size) {
-    Allocator* a = (Allocator*)allocator;
-    if(a->allocation_count >= ALLOCATOR_MAX_ALLOCS) {
-        return NULL;
-    }
-
-    void* new_alloc = malloc(size);
-
-    if(new_alloc) {
-        a->allocations[a->allocation_count] = (Allocation){ .adress = new_alloc, .size = size };
-        a->allocation_count++;
-        a->full_size += size;
-    }
-
-    TEST_ALLOCATOR_RUN(printf("New alloc(%p): %zu\n", new_alloc, size));
-    return new_alloc;
-}
-
-static void* allocator_realloc(void* allocator, void* mem_ptr, size_t new_size) {
-    Allocator* a = (Allocator*)allocator;
-
-    void* new_alloc = realloc(mem_ptr, new_size);
-
-    for(size_t i = 0; i < a->allocation_count; i++) {
-        Allocation* allocation = &a->allocations[i];
-        if(allocation->adress == mem_ptr) {
-            TEST_ALLOCATOR_RUN(printf("Changed alloc(%p->%p): %zu->%zu bytes\n", mem_ptr, new_alloc, allocation->size, new_size);)
-
-            if(new_alloc) {
-                a->full_size += new_size;
-                a->full_size -= allocation->size;
-                *allocation = (Allocation){ .adress = new_alloc, .size = new_size };
-            }
-            return new_alloc;//early return
-        }
-    }
-    
-    if(new_alloc) {
-        if(a->allocation_count >= ALLOCATOR_MAX_ALLOCS) {
-            free(new_alloc);
-            return NULL;
-        }
-
-        a->allocations[a->allocation_count] = (Allocation){ .adress = new_alloc, .size = new_size };
-        a->allocation_count++;
-        a->full_size += new_size;
-
-        TEST_ALLOCATOR_RUN(printf("New alloc from realloc(%p): %zu\n", new_alloc, new_size);)
-    }
-    return new_alloc;
-}
-
-static void allocator_free(void* allocator, void* mem_ptr) {
-    Allocator* a = (Allocator*)allocator;
-
-    free(mem_ptr);
-
-    bool found = false;
-    for(size_t i = 0; i < a->allocation_count; i++) {
-        Allocation* allocation = &a->allocations[i];
-        
-        if(!found) {
-            if(allocation->adress == mem_ptr) {
-                found = true;
-                a->full_size -= allocation->size;
-
-                TEST_ALLOCATOR_RUN(printf("Freed(%p): %zu\n", mem_ptr, allocation->size);)
-            }
-        }
-        
-        if(found && (i + 1) < a->allocation_count) {//copy adjacent allocation
-            a->allocations[i] = a->allocations[i + 1];
-        }
-    }
-    if(found) {
-        a->allocation_count--;
-    }
-}
-
-static Allocator test_allocator;
-
-#define TEST_ALLOCATOR (HF_CSV_AllocatorData){ &test_allocator, allocator_malloc, allocator_realloc, allocator_free}
-#else
-#define TEST_ALLOCATOR NULL
-#endif
 
 int main(int argc, char* argv[]) {
     (void)argc;
     (void)argv;
-
-#ifdef USE_TEST_ALLOCATOR
-    test_allocator.allocation_count = 0;
-    test_allocator.full_size = 0;
-    test_allocator.allocations = malloc(sizeof(Allocation) * ALLOCATOR_MAX_ALLOCS);
-    memset(test_allocator.allocations, 0, sizeof(Allocation) * ALLOCATOR_MAX_ALLOCS);
-    hf_csv_set_allocator_data(TEST_ALLOCATOR);
-#endif
 
     //test creating a new csv from scratch
     HF_CSV* my_csv = hf_csv_create(3, 3);
@@ -187,7 +68,7 @@ int main(int argc, char* argv[]) {
     {//optional line feed test
         HF_CSV* line_feed = hf_csv_create_from_file("./res/line_feed.csv");
         assert(line_feed);
-        
+
         char* line_feed_str = hf_csv_to_string(line_feed);
         printf("\n\nline_feed csv:\n%s\n", line_feed_str);
         hf_csv_free_string(line_feed_str);
@@ -223,14 +104,6 @@ int main(int argc, char* argv[]) {
 
         hf_csv_destroy(line_feed_csv);
     }
-
-#ifdef USE_TEST_ALLOCATOR
-    printf("%zu allocations not freed: %zu bytes\n", test_allocator.allocation_count, test_allocator.full_size);
-    for(size_t i = 0; i < test_allocator.allocation_count; i++) {
-        printf("NOT FREED(%p): %zu bytes\n", test_allocator.allocations[i].adress, test_allocator.allocations[i].size);
-    }
-    free(test_allocator.allocations);
-#endif
 
     return 0;
 }
